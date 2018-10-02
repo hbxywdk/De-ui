b32fd76faf1ac124498142f78fb60391883515ab
hbxywdk <364746078@qq.com>
2018-10-02 13:58:49 +0800

    分割线组建

---
+++ components/de-line/README.md
@@ -0,0 +1,6 @@
+属性 | 类型 | 说明 | 默认值
+:- | :-: | :-: | -: 
+lineColor | String | 颜色 | #2ccaf4
+text | String | 线上文字内容，不传则为纯分割线 | ''
+lineWidth | String | 分割线宽度（px、rpx、百分比） | auto
+
---
+++ components/de-line/de-line.js
@@ -0,0 +1,27 @@
+Component({
+
+  properties: {
+    // 颜色
+    lineColor: {
+      type: String,
+      value: '#2ccaf4'
+    },
+    // 文本
+    text: {
+      type: String,
+      value: ''
+    },
+    lineWidth: {
+      type: String,
+      value: 'auto'
+    }
+  },
+
+  data: {
+
+  },
+
+  methods: {
+
+  }
+})
