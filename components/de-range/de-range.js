d92a7d20b6e6137fe9d57eaa38cc0a0427c079f7
hbxywdk <364746078@qq.com>
2018-10-13 22:20:55 +0800

    range组件

--- components/de-range/de-range.js
+++ components/de-range/de-range.js
@@ -2,7 +2,7 @@
 Component({
 
   properties: {
-    // 类别 single：单滑块 dual：双滑块
+    // 类别 single：单滑块 dual：双滑块（暂只做单滑块）
     type: {
       type: String,
       value: 'single'
@@ -17,13 +17,14 @@ Component({
       type: Number,
       value: 100
     },
+    // 初始值
+    value: {
+      type: Number,
+      value: 0
+    },
     lineColor: {
       type: String,
       value: '#2ccaf4'
-    },
-    lineWeight: {
-      type: String,
-      value: '10rpx'
     },
     disabled: {
       type: Boolean,
@@ -35,31 +36,39 @@ Component({
     // de-range的信息
     width: 0,
     left: 0,
+    // value: 0, // 值 
     // 最小值
     originLeft: 0, // 原始Left
     minStart: 0, // touch起始Left
-    minLeft: 0, // Left
-    // 最大值
-    maxStart: 0, // touch起始Left
-    maxLeft: 0, // Left
+    minLeft: 0, // 实时Left
+    // 最大值（预留双滑块）
+    // maxStart: 0, // touch起始Left
+    // maxLeft: 0, // 实时Left
   },
   ready () {
+    const value = this.properties.value
     const query = wx.createSelectorQuery().in(this)
     query.select('.de-range').boundingClientRect(res => {
-      console.log(res)
-      this.setData({ 
-        width: res.width,
-        left: res.left
-      })
+      // console.log(res)
+      let width = res.width
+      let left = res.left
+      if (value && value >= this.properties.min && value <= this.properties.max) {
+        let leftValue = res.width * (value / (this.properties.max - this.properties.min))
+        this.setData({ originLeft: leftValue, minStart: leftValue, minLeft: leftValue })
+        
+      }
+      this.setData({ width, left })
     }).exec()
   },
 
   methods: {
     touchStart (e) {
+      if (this.properties.disabled) return
       console.log('start', e.changedTouches[0].pageX)
       this.setData({ minStart: e.changedTouches[0].pageX })
     },
     touchMove(e) {
+      if (this.properties.disabled) return
       let minLeft = Math.round(e.changedTouches[0].pageX - this.data.minStart + this.data.originLeft)
       if (minLeft < 0) {
         minLeft = 0
@@ -68,9 +77,16 @@ Component({
         minLeft = this.data.width
       }
       // console.log(e.changedTouches[0].pageX)
-      this.setData({ minLeft })
+      // console.log(minLeft / this.data.width, '---', this.properties.max - this.properties.min)
+      const value = Math.round((minLeft / this.data.width) * (this.properties.max - this.properties.min)) + this.properties.min
+      this.setData({ 
+        minLeft,
+        // value
+      })
+      this.triggerEvent('change', value)
     },
     touchEnd(e) {
+      if (this.properties.disabled) return
       let left = Math.round(e.changedTouches[0].pageX - this.data.minStart + this.data.originLeft)
       if (left < 0) {
         left = 0
--- pages/Function/range/range.js
+++ pages/Function/range/range.js
@@ -1,66 +1,29 @@
 // pages/Function/range/range.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    val1: 0,
+    val2: 0,
+    val3: 0,
+    val4: 0,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  change1 (e) {
+    this.setData({
+      val1: e.detail
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  change2 (e) {
+    this.setData({
+      val2: e.detail
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  change3(e) {
+    this.setData({
+      val3: e.detail
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  change4(e) {
+    this.setData({
+      val4: e.detail
+    })
   }
 })
