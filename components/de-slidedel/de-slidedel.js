3993e5b427d0f6b28dc77f0c3400af8d2de9fc99
hbxywdk <364746078@qq.com>
2018-11-02 23:50:33 +0800

    coding slidedel

--- components/de-slidedel/de-slidedel.js
+++ components/de-slidedel/de-slidedel.js
@@ -10,7 +10,8 @@ Component({
   data: {
     areaWidth: 0, // 按钮区宽度
     translateX: 0, // X轴位移距离
-    startPoint: '', // 起始点
+    startX: '', // 起始点
+    isMoving: false, // 滑块是否在动画中
   },
   
   ready () {
@@ -20,18 +21,21 @@ Component({
       this.setData({
         areaWidth: res.width
       })
+      console.log('滑块宽度', res.width)
     }).exec()
   },
 
   methods: {
     start (e) {
       if (this.properties.disabled) return
-      console.log('start', e.changedTouches[0].pageX)
-      this.setData({ startPoint: e.changedTouches[0].pageX })
+      const startX = e.changedTouches[0].pageX
+      console.log('startX', startX)
+      this.setData({ startX })
     },
     move (e) {
       if (this.properties.disabled) return
       const x = e.changedTouches[0].pageX
+      console.log('move', x)
       let translateX = x - this.data.startPoint > 0 ? 0 : x - this.data.startPoint
       translateX = translateX < -this.data.areaWidth ? -this.data.areaWidth : translateX
       console.log('translateX', translateX)
@@ -42,7 +46,15 @@ Component({
     },
     end (e) {
       if (this.properties.disabled) return
-      console.log('end', e.changedTouches[0].pageX)
+      const endX = e.changedTouches[0].pageX
+      console.log('endX', endX)
+      // 滑动距离超出2/3，则自动划出
+      if ((endX - this.data.startX) > Math.ceil(this.data.areaWidth/3*2)) {
+        
+      // 自动滑回
+      } else {
+
+      }
     }
   }
 })
